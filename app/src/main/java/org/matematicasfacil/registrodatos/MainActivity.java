bbb3f76ade4c55cb2dbfc02ca4e029e5e4c43b2c
hipolito <huaylla7@hotmail.com>
2023-06-26 12:46:31 -0400

    Inicializado Binding

--- app/src/main/java/org/matematicasfacil/registrodatos/MainActivity.java
+++ app/src/main/java/org/matematicasfacil/registrodatos/MainActivity.java
@@ -7,13 +7,47 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.matematicasfacil.registrodatos.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        //Método ViewBinding
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = String.valueOf(binding.editTextNombre.getText());
+                String lastName = binding.editTextApellido.getText().toString();
+                String email = binding.editTextEmail.getText().toString();
+                String password = binding.editTextPassword.getText().toString();
+
+                if(nombre.getText().toString().isEmpty() || apellido.getText().toString().isEmpty() || correo.getText().toString().isEmpty()
+                        || clave.getText().toString().isEmpty()   ){
+                    Toast.makeText(getBaseContext(), "Faltan campos por completar ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!correo.getText().toString().contains("@")){
+                    Toast.makeText(getBaseContext(), "Campo correo invalido ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String concatenado = "Nombre: " + name + " Apellido: " + lastName + " Correo: " + email + " Contraseña: " + password;
+                Toast.makeText(getBaseContext(), concatenado, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        //MÉTODO findViewById()
+        /*
+        setContentView(R.layout.activity_main);
         EditText nombre = findViewById(R.id.editTextNombre);
         EditText apellido = findViewById(R.id.editTextApellido);
         EditText correo = findViewById(R.id.editTextEmail);
@@ -41,6 +75,6 @@ public class MainActivity extends AppCompatActivity {
                 String concatenado = "Nombre: " + name + " Apellido: " + lastName + " Correo: " + email + " Contraseña: " + password;
                 Toast.makeText(getBaseContext(), concatenado, Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
     }
 }
